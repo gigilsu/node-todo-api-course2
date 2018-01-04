ef1495fb6ba6febb124d8f07e2ee453dca85932d
gigilsu <gigi.lsu@gmail.com>
2018-01-03 18:59:13 -0600

    setup separate test and dev envs

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+require('./config/config');
+
 const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -8,7 +10,7 @@ const {Todo} = require('./models/todo');
 const {User} = require('./models/user');
 
 var app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 app.use(bodyParser.json());
 
@@ -94,7 +96,7 @@ app.delete('/todos/:id', (req, res) => {
         return res.status(404).send();
       }
 
-      res.send({todo})      
+      res.send({todo})
     }).catch((e) => {
       res.status(400).send();
     })
