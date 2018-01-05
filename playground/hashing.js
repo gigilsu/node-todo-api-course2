ae85193f4bcabea4c5f7b28016a2437f9229a673
gigilsu <gigi.lsu@gmail.com>
2018-01-04 18:19:45 -0600

    Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,5 +1,30 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+
+var password = '123abc!';
+
+bcrypt.genSalt(10, (err, salt) => {
+  bcrypt.hash(password, salt, (err, hash) => {
+    console.log(hash);
+  });
+});
+
+var hashedPassword = '$2a$10$KPZT8R0nppjfi83YD1OcNuktE45pGfYNymAWdS55fX329LKPTyv0K';
+var hashedTwo = '$2a$10$lFIZXtNtsP1PTaWk7OGMn.ahK22WBbJyHuSwDA5UEroUyUyzl5fkW';
+var hashedThree = '$2a$10$O04q9GlB9dxRcjgXDFcVkurIBpyDvGHmlWlznP8xOZgGMOVJYMLjK';
+
+bcrypt.compare(password, hashedPassword, (err, res) => {
+  console.log(res);
+});
+
+bcrypt.compare(password, hashedTwo, (err, res) => {
+  console.log(res);
+});
+
+bcrypt.compare(password, hashedThree, (err, res) => {
+  console.log(res);
+});
 
 // jwt.sign
 // jwt.verify
@@ -10,15 +35,15 @@ const jwt = require('jsonwebtoken');
 // console.log(`Message: ${message}`);
 // console.log(`Hash: ${hash}`);
 //
- var data = {
-   id: 10
- };
-
- var token = jwt.sign(data, '123abc');
- console.log(token);
-
-var decoded = jwt.verify(token, '123abc');
-console.log('decoded: ', decoded);
+//  var data = {
+//    id: 10
+//  };
+//
+//  var token = jwt.sign(data, '123abc');
+//  console.log(token);
+//
+// var decoded = jwt.verify(token, '123abc');
+// console.log('decoded: ', decoded);
 //
 // var token = {
 //   data,
